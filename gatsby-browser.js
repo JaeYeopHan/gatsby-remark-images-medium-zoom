21f04169168df2a1e6debe030747503051c18089
Alexey Pyltsyn <lex61rus@gmail.com>
2019-10-02 14:41:55 +0300

    Improve applyZoomEffect function

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -58,19 +58,22 @@ function injectStyles(options) {
   document.head.appendChild(node)
 }
 
-function applyZoomEffect(options) {
-  const images = Array.from(
-    document.querySelectorAll(`${imageClass}:not(${options.excludedSelector})`)
-  ).map(el => {
-    function onImageLoad() {
-      const originalTransition = el.style.transition
+function applyZoomEffect({ excludedSelector, ...options }) {
+  const imagesSelector = excludedSelector
+    ? `${imageClass}:not(${excludedSelector})`
+    : imageClass
+  const images = Array.from(document.querySelectorAll(imagesSelector)).map(
+    el => {
+      function onImageLoad() {
+        const originalTransition = el.style.transition
 
-      el.style.transition = `${originalTransition}, ${TRANSITION_EFFECT}`
-      el.removeEventListener('load', onImageLoad)
+        el.style.transition = `${originalTransition}, ${TRANSITION_EFFECT}`
+        el.removeEventListener("load", onImageLoad)
+      }
+      el.addEventListener("load", onImageLoad)
+      return el
     }
-    el.addEventListener('load', onImageLoad)
-    return el
-  })
+  )
 
   if (images.length > 0) {
     mediumZoom(images, options)
